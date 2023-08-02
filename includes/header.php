2388c9f3dcacd530d55b89e6e59737441cdcedb9
AshaDesai8283 <140842886+AshaDesai8283@users.noreply.github.com>
2023-08-02 18:11:17 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    <a class="navbar-brand mx-auto d-lg-none" href="index.php">
+                        WELLABLE
+                        
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <img src="img2.png" style =" border-radius: 50%; height: 50px;">
+                        <a class= "navbar-brand d-none d-lg-block" href="index.php">
+                                WELLABLE
+                                <strong class="d-block"></strong>
+                            </a>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                             <li class="nav-item active">
+                                <a class="nav-link" href="doctor/signup.php">Doctor</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
